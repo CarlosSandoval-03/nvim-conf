22bad335edd710c2c24706f76f53f4aa163dbc36
Carlos Sandoval <csandovalc@unal.edu.co>
2023-01-21 23:15:54 -0500

    New: Plugin manager, fuzzy finder, and colors

---
+++ after/plugin/colors.lua
@@ -0,0 +1,11 @@
+function applyColor(color)
+	color = color or "tokyonight-night"
+	vim.cmd.colorscheme(color)
+end
+
+function applyTransparency()
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+applyColor()
